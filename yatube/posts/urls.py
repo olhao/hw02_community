5772e52def70f33c33ec4948cf36b8f96a65a19a
Olha <orukshyna@gmail.com>
2021-07-18 22:35:46 +0200

    Added yatube project

---
+++ yatube/posts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'posts'
+
+urlpatterns = [
+    # Главная страница
+    path('', views.index, name='index'),
+    path('group/<slug>/', views.group_posts, name='group_list')
+]
