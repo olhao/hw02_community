7eeefc7c4f9464a88f5828a8f188c694384831bc
Olha <orukshyna@gmail.com>
2021-07-21 00:29:28 +0200

    Modified slug type in url (did not work as slug:slug - failed test)

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,5 +7,5 @@ app_name = 'posts'
 urlpatterns = [
     # Главная страница
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_list')
+    path('group/<slug>/', views.group_posts, name='group_list')
 ]
