022c03e2990ea72066e4068ea52df694b89ce1f4
Olha <orukshyna@gmail.com>
2021-07-21 20:27:17 +0200

    Modified according to review

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -17,9 +17,6 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-admin.site.register(Post, PostAdmin)
-
-
 class GroupAdmin(admin.ModelAdmin):
     list_display = (
         'pk',
@@ -32,4 +29,5 @@ class GroupAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+admin.site.register(Post, PostAdmin)
 admin.site.register(Group)
