ccb0b49ead6116c10360cb07c41a03cbec89e00c
Olha <orukshyna@gmail.com>
2021-07-22 14:13:28 +0200

    Modified according to review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
-        related_name='groups')
+        related_name='posts')
 
     class Meta:
         ordering = ['-pub_date']
